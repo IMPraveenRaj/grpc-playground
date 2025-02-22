2d1f694943a0ba5fa31f7c1a62b440fe2c50ee9a
IMPraveenRaj <impraveen13@gmail.com>
2025-02-22 23:43:35 +0100

    setting up for the grpc

--- src/main/java/com/springheaven/grpc/App.java
+++ src/main/java/com/springheaven/grpc/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "i am going to be a pro in the programming  World!" );
+        System.out.println("Feeling Grateful gratitude love you");
     }
 }
